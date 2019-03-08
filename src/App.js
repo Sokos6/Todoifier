6f43878fc3764de62439dc40e8585ffc0e995042
Will Sokolowski <will.sokolowski@gmail.com>
2019-03-08 16:12:42 -0700

    todo component and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Todo from './Todo';
 
 const App = () => (
   <div className="App">
     <h2>Todoifier</h2>
+    <br />
+    <Todo />
+    <Todo />
   </div>
 );
 
