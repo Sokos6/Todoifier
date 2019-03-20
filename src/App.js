2fb66f9b71d9f1918cf19391fd054c923d8b4fba
Will Sokolowski <will.sokolowski@gmail.com>
2019-03-20 16:55:20 -0700

    TodoList and CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import Todo from './Todo';
+import TodoList from './TodoList';
 
 const App = () => (
   <div className="App">
     <h2>Todoifier</h2>
     <br />
-    <Todo description="Do the thing" />
-    <Todo description="Do another thing" />
+    <TodoList />
   </div>
 );
 
